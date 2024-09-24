42548dc968139e3730e0917a0b421f1f39624456
sebastianpineda <spineda@mit.edu>
2024-09-23 22:45:54 -0700

    Update config

--- src/wr_experimental.cpp
+++
@@ -1,7 +0,0 @@
-// Rcpp interface for `action` module
-// Organized by module header in th order imported.
-// [[Rcpp::interfaces(r, cpp)]]
-
-#include "actionet_r_config.h"
-
-// experimental ==================================================================================================================
