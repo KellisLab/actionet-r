c63b384ae5977c59a3a2d6bfb8e64939794ed65a
sebastianpineda <sebastianpineda95@gmail.com>
2026-02-27 23:57:40 +0000

    sync bindings

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -230,9 +230,16 @@ C_orthogonalizeBasal_full <- function(S, old_S_r, old_V, old_A, old_B, old_sigma
 #' @param k Dimension of SVD decomposition
 #' @param max_it Number of iterations (default=5)
 #' @param seed Random seed (default=0)
-#' @param algorithm SVD algorithm to use. Currently supported methods are blah blah blah
+#' @param algorithm SVD algorithm to use:
+#'   - 0 = IRLB (default, good for small/medium matrices)
+#'   - 1 = Halko (randomized SVD)
+#'   - 2 = Feng (another randomized method)
+#'   - 3 = PRIMME (NOT AVAILABLE in R - use Python for large matrices)
 #'
 #' @return A named list with U, sigma, and V components
+#'
+#' @note PRIMME algorithm is not available in R builds due to R's 32-bit matrix limitations.
+#'   For very large sparse matrices (>2^31 elements), use the Python package.
 #'
 #' @examples
 #' A = randn(100, 20)
